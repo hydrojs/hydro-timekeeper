ed60af7cabe27723f7e246eaea06e9926a9300e2
Veselin Todorov <veselin@veselin.bg>
2013-12-11 19:05:48 +0200

    Make tests less fragile

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ var date = 'Wed Dec 11 2013 18:23:44 GMT+0200 (EET)';
 
 test('set time', function() {
   timekeeper.freeze(date);
-  assert((new Date).toString() === date);
+  assert(+(new Date) === +(new Date(date)));
 });
 
 test('reset time', function() {
-  assert((new Date).toString() !== date);
+  assert(+(new Date) !== +(new Date(date)));
 });
